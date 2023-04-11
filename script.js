1c34fa99f1a5f8907c85e8fbbc409af2ebf46c4b
magatdarwin <magat.darwin@gmail.com>
2023-04-11 10:32:58 +0800

    Add gameBoxes array and bind DOM elements to it

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 const gameBoard = (doc => {
+  let gameBoxes = [];
+
   const draw = () => {
     const body = doc.querySelector('body');
     const outerBox = doc.createElement('div');
     outerBox.classList.add('outer-box');
 
     let box;
-    for (let i = 0; i < 9; i++) {
-      box = doc.createElement('div');
-      box.classList.add('box');
+    for (let row = 0; row < 3; row++) {
+      gameBoxes.push([]);
+      for (let column = 0; column < 3; column++) {
+        box = doc.createElement('div');
+        box.classList.add('box');
 
-      if (i >= 3 && i <= 5) {
-        box.classList.add('row-2');
+        box.dataset.row = row;
+        box.dataset.column = column;
+
+        if (row === 1) {
+          box.classList.add('row-2');
+        }
+
+        if (column === 1) {
+          box.classList.add('col-2');
+        }
+
+        gameBoxes[row].push('');
+        outerBox.appendChild(box);
       }
-
-      if (i === 1 || i === 4 | i === 7) {
-        box.classList.add('col-2');
-      }
-
-      outerBox.appendChild(box);
     }
 
     body.appendChild(outerBox);
   }
 
   return {
+    gameBoxes,
     draw,
   }
 })(document);
