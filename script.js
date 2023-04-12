83211ad38653fec84cd10d971883ff05ebf85f61
magatdarwin <magat.darwin@gmail.com>
2023-04-12 16:08:00 +0800

    Add displayController to handle adding symbols to the DOM

--- script.js
+++ script.js
@@ -22,3 +22,43 @@ const gameBoard = (() => {
     setSymbol,
   }
 })();
+
+const displayController = (doc => {
+  const drawBoard = () => {
+    const body = doc.querySelector('body');
+    const outerBox = doc.createElement('div');
+    outerBox.classList.add('outer-box');
+
+    let box;
+    for (let row = 0; row < 3; row++) {
+      for (let column = 0; column < 3; column++) {
+        box = doc.createElement('div');
+        box.classList.add('box');
+
+        box.dataset.row = row;
+        box.dataset.column = column;
+
+        if (row === 1) {
+          box.classList.add('row-2');
+        }
+
+        if (column === 1) {
+          box.classList.add('col-2');
+        }
+
+        outerBox.appendChild(box);
+      }
+    }
+
+    body.appendChild(outerBox);
+  }
+
+  const drawSymbol = (symbol, target) => {
+    target.innerText = symbol;
+  }
+
+  return {
+    drawBoard,
+    drawSymbol,
+  }
+})(document);
