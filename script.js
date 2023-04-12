cf13962c96f43379974780ce8713b4fdce88de0d
magatdarwin <magat.darwin@gmail.com>
2023-04-12 15:59:08 +0800

    Remove document parameter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (doc => {
+const gameBoard = (() => {
   let boxes = [];
 
   const populateBoxes = () => {
@@ -21,4 +21,4 @@ const gameBoard = (doc => {
     getSymbol,
     setSymbol,
   }
-})(document);
+})();
